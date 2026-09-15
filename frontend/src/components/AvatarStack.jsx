7d442fcede001dc9c098f38b91833ddd5fa9c853
danchomas <danchomas@gmail.com>
2026-09-15 11:54:24 +0300

    remove unnecessary files

--- frontend/src/components/AvatarStack.jsx
+++
@@ -1,9 +0,0 @@
-export default function AvatarStack({ colors = ["a1", "a2", "a3"] }) {
-  return (
-    <div className="avatar-stack">
-      {colors.map((colorClass, index) => (
-        <span key={index} className={colorClass}></span>
-      ))}
-    </div>
-  );
-}
